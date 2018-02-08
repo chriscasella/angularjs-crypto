e2a23352844f7368939f8c182eff80bbc3f1eae6
chriscasella <chris.casella@gmail.com>
2018-02-07 21:26:31 -0500

    folders and files. Coin ctrl and view

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ app.config(['$locationProvider', '$routeProvider', function ($locationProvider, 
 
     $routeProvider
     .when('/',{
-        templateUrl: 'home.html',
+        templateUrl: 'home/home.html',
         controller: 'HomeController'
     })
     .when('/currencies',{
-        templateUrl: 'currencies.html',
+        templateUrl: 'currencies/currencies.html',
         controller: 'CurrenciesController'
+    })
+    .when('/currencies/:coin',{
+        templateUrl: 'coin/coin.html',
+        controller: 'CoinController',
+        data:{
+            data : 'coin'
+        }
     })
     .otherwise({ redirectTo: '/' });
 }]);
